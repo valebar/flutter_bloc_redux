6661e1cecdc788f25ca381cd3d4422f32c80039e
valebar <vasja.lebar@my-healthydog.com>
2019-09-27 16:23:49 +0200

    manjši propravki

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -10,66 +10,67 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StoreConnector(
-        converter: (store) => PageViewModel.fromStore(store),
-        builder: (BuildContext context, PageViewModel vm) {
-          CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
-          return Scaffold(
-            appBar: AppBar(
-              title: Text("First page"),
-            ),
-            body: Center(
-              child: MultiBlocListener(
-                listeners: [
-                  BlocListener(
-                    bloc: counterBloc,
-                    listener: (BuildContext context, state) {
-                      if (state is IncrementCounterState) {
-                        vm.increment();
-                      }
-                      if (state is DecrementCounterState) {
-                        vm.decrement();
-                      }
+      converter: (store) => PageViewModel.fromStore(store),
+      builder: (BuildContext context, PageViewModel vm) {
+        CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("First page"),
+          ),
+          body: Center(
+            child: MultiBlocListener(
+              listeners: [
+                BlocListener(
+                  bloc: counterBloc,
+                  listener: (BuildContext context, state) {
+                    if (state is IncrementCounterState) {
+                      vm.increment();
+                    }
+                    if (state is DecrementCounterState) {
+                      vm.decrement();
+                    }
+                  },
+                )
+              ],
+              child: Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      counterBloc.dispatch(
+                        DecrementCounterEvent(),
+                      );
+                    },
+                  ),
+                  Row(
+                    children: [
+                      Text("Counter: ${vm.counter}"),
+                    ],
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      counterBloc.dispatch(
+                        DecrementCounterEvent(),
+                      );
+                    },
+                  ),
+                  MaterialButton(
+                    child: Text("Second Page"),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => SecondPage(),
+                        ),
+                      );
                     },
                   )
                 ],
-                child: Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.remove),
-                      onPressed: () {
-                        counterBloc.dispatch(
-                          DecrementCounterEvent(),
-                        );
-                      },
-                    ),
-                    Row(
-                      children: [
-                        Text("Counter: ${vm.counter}"),
-                      ],
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () {
-                        counterBloc.dispatch(
-                          DecrementCounterEvent(),
-                        );
-                      },
-                    ),
-                    MaterialButton(
-                      child: Text("Second Page"),
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => SecondPage(),
-                          ),
-                        );
-                      },
-                    )
-                  ],
-                ),
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
